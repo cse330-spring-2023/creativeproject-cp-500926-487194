78647c79c34bd584c064afd6b0a34a54bf591af7
franciscomiguens <f.miguens@wustl.edu>
2023-04-13 14:21:39 -0500

    some homepage changes

--- client/src/Homepage.js
+++ client/src/Homepage.js
@@ -1,24 +1,45 @@
-import { useState,useEffect,Component } from 'react';
-import axios from 'axios'
-import {useHistory} from 'react-router-dom'
+import { useState, useEffect, Component } from "react";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
 
+function Homepage({ userData }) {
+  const [userList, setUserList] = useState([]);
 
-function Homepage({userData}) {
-    
-    const [userList,setUserList] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:1234/api/get").then((data) => {
+      setUserList(data.data);
+    });
+  }, []);
 
+  return (
+    <>
+      <h1> Hello {/* displayName of user */}!</h1>
+      <div id="nav">
+        {/* this is meant to be a navigation bar. userProfile will be inline */}
+        <div id="userProfile">
+          <p>Profile</p>
+        </div>
+        <div id="explore">
+          <p>Explore</p>
+        </div>
+      </div>
+      <div id="mainPage">
+        {/* This is what react is going to edit between toggling pages. Have to learn how to do this i guess. Since this is the home page
+        I am going to make it look like the profile page */}
+        <div id="topSongs">
+          <div id="song1"></div>
+          <div id="song2"></div>
+          <div id="song3"></div>
+          <div id="song4"></div>
+          <div id="song5"></div>
+          <div id="votes"></div>
+        </div>
+        <div id="settings">
+          {/* not sure if to just show every setting or if to have it pop down could be a good spot for animation */}
+        </div>
+      </div>
 
-        useEffect(()=>{
-            axios.get("http://localhost:1234/api/get").then((data)=>{
-                setUserList(data.data)
-            });
-            },[])
-    
-
-
-
-    return (
-        <>
+      {/*
             <p> Hello! </p>
             <img src="./evan.jpg"></img>
             <h2>Users: </h2>
@@ -36,9 +57,9 @@ function Homepage({userData}) {
                 ))}
             </ul>
             <p>End of Users </p>
-        </>
-    )
-
+        */}
+    </>
+  );
 }
 
 export default Homepage;
